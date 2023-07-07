876259f6d7577508ba814c94bcb713154e8ea2b6
Daniel <gendik38@gmail.com>
2023-07-07 09:52:39 +0200

    Implement whatsdifferent section

---
+++ app/components/WhatsDifferent/WhatsDifferent.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const WhatsDifferent = () => {
+	return (
+		<section className="flex flex-col px-10 items-center py-10 gap-5">
+			<h2 className="text-dark-blue font-extrabold leading-[45px] tracking-[-0.619px] text-3xl text-center">
+				What's different about Manage ?
+			</h2>
+			<p className="text-dark-blue text-center text-lg font-normal opacity-[0.5025343894958496;]">
+				Manage provides all the functionality your team needs, without the
+				complexity. Our software is tailor-made for modern digital product
+				teams.{" "}
+			</p>
+		</section>
+	)
+}
+
+export default WhatsDifferent
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import WhatsDifferent from './components/WhatsDifferent/WhatsDifferent'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
    <Header />
    <main>
    <Hero />
+   <WhatsDifferent />
 
    </main>
    </>
